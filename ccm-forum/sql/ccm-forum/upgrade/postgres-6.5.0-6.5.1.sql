02b52f11ac6be32720c758e23f7849ba14d1d8d8
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2008-06-22 13:28:30 +0000

    small bugfix for r56 sql upgrade script (vendor r1707)
    git-svn-id: https://svn.libreccm.org/ccm/trunk@57 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-forum/sql/ccm-forum/upgrade/postgres-6.5.0-6.5.1.sql
+++ ccm-forum/sql/ccm-forum/upgrade/postgres-6.5.0-6.5.1.sql
@@ -21,14 +21,14 @@
  
 begin;
 
-\i ./postgres/6.5.0-6.5.1/add_privileges.sql
-\i ./postgres/6.5.0-6.5.1/add_groups.sql
-\i ./postgres/6.5.0-6.5.1/add_thread_subscriber.sql
-\i ./postgres/6.5.0-6.5.1/add_file_attachments.sql
-\i ./postgres/6.5.0-6.5.1/add_forum_introduction.sql
-\i ./postgres/6.5.0-6.5.1/add_image_uploads.sql
-\i ./postgres/6.5.0-6.5.1/add_forum_no_category_posts.sql
-\i ./postgres/6.5.0-6.5.1/add_anonymous_option.sql
+\i postgres/6.5.0-6.5.1/add_privileges.sql
+\i postgres/6.5.0-6.5.1/add_groups.sql
+\i postgres/6.5.0-6.5.1/add_thread_subscriber.sql
+\i postgres/6.5.0-6.5.1/add_file_attachments.sql
+\i postgres/6.5.0-6.5.1/add_forum_introduction.sql
+\i postgres/6.5.0-6.5.1/add_image_uploads.sql
+\i postgres/6.5.0-6.5.1/add_forum_no_category_posts.sql
+\i postgres/6.5.0-6.5.1/add_anonymous_option.sql
 
 commit;
 
