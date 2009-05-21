65cfd085e24092527fd8831af48fb7c5d185c31d
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2009-05-21 08:38:57 +0000

    Minor change to OrganizationFunction. OrganizationFunction extends now the same class in PDL and Java.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@174 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms-types-genericorganization/src/com/arsdigita/cms/contenttypes/OrganizationFunction.java
+++ ccm-cms-types-genericorganization/src/com/arsdigita/cms/contenttypes/OrganizationFunction.java
@@ -8,14 +8,16 @@
 
 package com.arsdigita.cms.contenttypes;
 
+import com.arsdigita.cms.ContentPage;
 import com.arsdigita.domain.DomainObject;
 import com.arsdigita.persistence.DataObject;
-import com.arsdigita.persistence.metadata.ObjectType;
+import com.arsdigita.domain.DataObjectNotFoundException;
 import com.arsdigita.persistence.OID;
+import java.math.BigDecimal;
 import org.apache.log4j.Logger;
 
 
-public class OrganizationFunction extends DomainObject {
+public class OrganizationFunction extends ContentPage {
     
     private static final Logger s_log = Logger.getLogger(OrganizationFunction.class);
 
@@ -23,27 +25,32 @@ public class OrganizationFunction extends DomainObject {
 
     public static final String BASE_DATA_OBJECT_TYPE = "com.arsdigita.cms.contenttypes.OrganizationFunction";
 
+    public OrganizationFunction() {
+	super(BASE_DATA_OBJECT_TYPE);
+    }
+
+    public OrganizationFunction(BigDecimal id) throws DataObjectNotFoundException {
+	super(new OID(BASE_DATA_OBJECT_TYPE, id));
+    }
+
     public OrganizationFunction(String typeName) {
 	super(typeName);
     }
 
-    public OrganizationFunction(ObjectType type) {
-	super(type);
-    }
-
-    public OrganizationFunction(OID oid) {
+    public OrganizationFunction(OID oid) throws DataObjectNotFoundException {
 	super(oid);
-    }
+    } 
 
     public OrganizationFunction(DataObject dataObject) {
 	super(dataObject);
     }
 
+    //Accessors
     public String getFunctionName() {
 	return (String)get(FUNCTIONNAME);
     }
 
-    public void setFunctioName(String functionName) {
+    public void setFunctionName(String functionName) {
 	set(FUNCTIONNAME, functionName);
     }
 }
