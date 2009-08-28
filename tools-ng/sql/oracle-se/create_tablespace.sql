6b71eb3ec59d4145b73c2d62c69b89afc803d683
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2009-08-28 13:49:40 +0000

    tools-ng: integrated r1908,r1910,r1913,r1915-1919,r1926; esp.: upgrade target added. trunk hb is now in sync with vendor r1926
    git-svn-id: https://svn.libreccm.org/ccm/trunk@240 8810af33-2d31-482b-a856-94f89814c4df

---
+++ tools-ng/sql/oracle-se/create_tablespace.sql
@@ -0,0 +1,14 @@
+create tablespace aplaws
+  logging
+  datafile 'aplaws.dbf' 
+  size 32m 
+  autoextend on 
+  next 32m maxsize 2048m
+  extent management local;
+create tablespace aplaws
+  logging
+  datafile 'aplaws.dbf' 
+  size 32m 
+  autoextend on 
+  next 32m maxsize 2048m
+  extent management local;
