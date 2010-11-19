b829c26dab13d5041c4761756889f1860815b1b9
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2010-11-19 16:15:58 +0000

    SciOrgamization
    RelatedLinkPropertiesStep angepaßt auf die neue Variante
    
    Quasi
    
    git-svn-id: https://svn.libreccm.org/ccm/trunk@627 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-sci-types-organization/src/com/arsdigita/cms/contenttypes/ui/SciProjectPublicationsStep.java
+++ ccm-sci-types-organization/src/com/arsdigita/cms/contenttypes/ui/SciProjectPublicationsStep.java
@@ -4,11 +4,9 @@
  */
 package com.arsdigita.cms.contenttypes.ui;
 
-import com.arsdigita.bebop.FormSection;
 import com.arsdigita.cms.ContentType;
 import com.arsdigita.cms.ItemSelectionModel;
 import com.arsdigita.cms.contentassets.ui.RelatedLinkPropertiesStep;
-import com.arsdigita.cms.contentassets.ui.RelatedLinkPropertyForm;
 import com.arsdigita.cms.ui.authoring.AuthoringKitWizard;
 
 /**
@@ -17,17 +15,12 @@ import com.arsdigita.cms.ui.authoring.AuthoringKitWizard;
  */
 public class SciProjectPublicationsStep extends RelatedLinkPropertiesStep {
 
-    public SciProjectPublicationsStep(ItemSelectionModel itemModel,
-                                      AuthoringKitWizard parent) {
-        super(itemModel, parent);
-    }
+    protected String m_linkListName = "SciProjectPublications";
+    protected ContentType m_contentType = ContentType.findByAssociatedObjectType(
+            "com.arsdigita.cms.contenttypes.Publication");
 
-    @Override
-    protected FormSection getEditSheet() {
-        return new RelatedLinkPropertyForm(getItemSelectionModel(),
-                                           getLinkSelectionModel(),
-                                           ContentType.
-                findByAssociatedObjectType(
-                "com.arsdigita.cms.contenttypes.Publication"));
+    public SciProjectPublicationsStep(ItemSelectionModel itemModel,
+            AuthoringKitWizard parent) {
+        super(itemModel, parent);
     }
 }
