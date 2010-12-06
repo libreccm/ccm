47729728d7f1be91ea1142384baba19b70f2ebdd
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2010-12-06 10:10:54 +0000

    SciAuthor im Publikationenmodul hinzugefügt. Bisher gabe es, wenn das Publikationenmodul ohne ccm-sci-types-publications oder ccm-cms-types-member installiert wird, kein im ContentCenter verwendbaren, von GenericPerson abgeleiteteten Typ.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@643 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-sci-publications/src/com/arsdigita/cms/contenttypes/SciAuthor.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2010 Jens Pelzetter,
+ * for the Center of Social Politics of the University of Bremen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package com.arsdigita.cms.contenttypes;
+
+import com.arsdigita.domain.DataObjectNotFoundException;
+import com.arsdigita.persistence.DataObject;
+import com.arsdigita.persistence.OID;
+import java.math.BigDecimal;
+
+/**
+ * A concrete class extending {@link GenericPerson}. Does not add any new
+ * fields, it just makes the internal type <code>GenericPerson</code> usable
+ * without any other modules.
+ *
+ * @author Jens Pelzetter
+ */
+public class SciAuthor extends GenericPerson {
+
+    public static final String BASE_DATA_OBJECT_TYPE =
+                               "com.arsdigita.cms.contenttypes.SciAuthor";
+
+    public SciAuthor() {
+        this(BASE_DATA_OBJECT_TYPE);
+    }
+
+    public SciAuthor(BigDecimal id) throws DataObjectNotFoundException {
+        this(new OID(BASE_DATA_OBJECT_TYPE, id));
+    }
+
+    public SciAuthor(OID oid) throws DataObjectNotFoundException {
+        super(oid);
+    }
+
+    public SciAuthor(DataObject dobj) {
+            super(dobj);
+    }
+
+    public SciAuthor(String type) {
+        super(type);
+    }
+}
---
+++ ccm-sci-publications/src/com/arsdigita/cms/contenttypes/ui/SciAuthorPropertyForm.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2010 Jens Pelzetter,
+ * for the Center of Social Politics of the University of Bremen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package com.arsdigita.cms.contenttypes.ui;
+
+import com.arsdigita.bebop.FormData;
+import com.arsdigita.bebop.event.FormInitListener;
+import com.arsdigita.bebop.event.FormSectionEvent;
+import com.arsdigita.bebop.event.FormSubmissionListener;
+import com.arsdigita.cms.ItemSelectionModel;
+import com.arsdigita.cms.contenttypes.SciAuthor;
+
+/**
+ * Form for editing the basic properties of a {@link SciAuthor}. This form
+ * extends the form for editing the basic properties of {@link GenericPerson}.
+ *
+ * @author Jens Pelzetter
+ * @see SciAuthor
+ * @see GenericPerson
+ * @see GenericPersonPropertyForm
+ */
+public class SciAuthorPropertyForm
+        extends GenericPersonPropertyForm
+        implements FormInitListener,
+                   FormSubmissionListener {
+
+    public SciAuthorPropertyForm(ItemSelectionModel itemModel) {
+        this(itemModel, null);
+    }
+
+    public SciAuthorPropertyForm(ItemSelectionModel itemModel,
+                                 SciAuthorPropertiesStep step) {
+        super(itemModel);
+    }
+
+    @Override
+    protected void addWidgets() {
+        super.addWidgets();
+    }
+
+    @Override
+    public void init(FormSectionEvent fse) {
+        super.init(fse);
+
+        super.initBasicWidgets(fse);
+    }
+
+    @Override
+    public void process(FormSectionEvent fse) {
+        super.process(fse);
+
+        SciAuthor author = (SciAuthor) super.processBasicWidgets(fse);
+        if ((author != null) && getSaveCancelSection().getSaveButton().
+                isSelected(fse.getPageState())) {
+            author.save();
+
+            init(fse);
+        }
+    }
+}
