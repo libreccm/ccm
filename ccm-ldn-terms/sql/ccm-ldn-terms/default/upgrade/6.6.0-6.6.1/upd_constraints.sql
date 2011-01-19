c278ef3fa377a5a1810fc3a15f5a2f0d0726a683
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2011-01-19 02:14:48 +0000

    UPDATE script ccm-ldn-terms.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@693 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-ldn-terms/sql/ccm-ldn-terms/default/upgrade/6.6.0-6.6.1/upd_constraints.sql
@@ -0,0 +1,22 @@
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public License
+-- as published by the Free Software Foundation; either version 2.1 of
+-- the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+-- $Id: upd_constraints.sql pboy $
+
+-- recreate constraint  on cms_articles which had to be deleted during
+-- update of cms_articles
+ALTER TABLE trm_domains_indexer
+      ADD CONSTRAINT trm_doma_inde_index_id_f_ggaqm FOREIGN KEY (indexer_id)
+      REFERENCES acs_objects(object_id);
