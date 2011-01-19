91ce336323d66545db3c8da661096ee1775f43bd
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2011-01-19 02:09:43 +0000

    UPDATE script for ccm type article: 6.6.0 ->  6.6.1. MUST RUN after ccm-cms update!
    git-svn-id: https://svn.libreccm.org/ccm/trunk@690 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-cms-types-article/sql/ccm-cms-types-article/upgrade/default/6.6.0-6.6.1/upd_table_ct_articles.sql
@@ -0,0 +1,22 @@
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public License
+-- as published by the Free Software Foundation; either version 2.1 of
+-- the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+-- $Id: upd_table_ct_articles.sql pboy $
+
+-- recreate constraint  on cms_articles which had to be deleted during
+-- update of cms_articles
+ALTER TABLE ct_articles
+      ADD CONSTRAINT ct_articles_item_id_f_6ofn1 FOREIGN KEY (item_id)
+      REFERENCES cms_articles(article_id);
