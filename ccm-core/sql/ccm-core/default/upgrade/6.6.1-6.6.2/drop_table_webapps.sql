c175af4662774ff4001ca0df97d58ae81603919a
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2011-04-06 10:24:50 +0000

    Noch kleinere Korrekturen an Update Scripten sowie ZeS Bundle.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@836 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/sql/ccm-core/default/upgrade/6.6.1-6.6.2/drop_table_webapps.sql
+++ ccm-core/sql/ccm-core/default/upgrade/6.6.1-6.6.2/drop_table_webapps.sql
@@ -1,6 +1,4 @@
 --
--- Copyright (C) 2011 Peter Boy All Rights Reserved.
---
 -- This library is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public License
 -- as published by the Free Software Foundation; either version 2.1 of
@@ -15,17 +13,6 @@
 -- License along with this library; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 --
--- $Id: ren_table_workspaces.sql pboy $
+-- $Id: drop_tables_acs_stylesheets.sql pboy $
 
-ALTER TABLE workspaces  drop constraint  workspace_workspace_id_p_vm9z2 CASCADE;
-ALTER TABLE workspaces  drop constraint  workspac_defau_layo_id_f_xvb7g;
-ALTER TABLE workspaces  drop constraint  workspace_workspace_id_f_dted3;
-ALTER TABLE workspaces  drop constraint  workspaces_owner_id_f_mbbra;
-ALTER TABLE workspaces  drop constraint  workspaces_party_id_f_jotdd;
-ALTER TABLE workspaces  drop constraint  workspaces_theme_id_f_tpdju;
-
-DROP INDEX workspaces_party_id_idx ;
-
-
-ALTER TABLE workspaces RENAME TO pw_workspaces ;
-
+drop table webapps;
