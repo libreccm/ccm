de5973956698cbb659e91ad7e2eec91d05428515
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2011-09-07 06:05:32 +0000

    Update Script ccm-cms zu r1081, Korrektur einer versehentlich gelöschten Zeile im Build Script.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1102 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-cms/sql/ccm-cms/default/upgrade/6.6.2-6.6.3/upd_table_persons.sql
@@ -0,0 +1,28 @@
+--
+-- Copyright (C) 2011 Peter Boy All Rights Reserved.
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public License
+-- as published by the Free Software Foundation; either version 2.1 of
+-- the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+-- $Id: upd_table_persons.sql pboy $
+
+-- This update is only applicable for the internal development tree at
+-- University of Bremen !  Don't use for the APLAWS main trunk on
+-- fedorahosted!
+
+ALTER TABLE cms_persons 
+    ADD COLUMN dabinId INTEGER;
+
+ALTER TABLE cms_persons 
+    ADD COLUMN aliasId INTEGER REFERNCES cms_persons;
