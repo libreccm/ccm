66a189a3044237b656965fe1264242bb0492cdcc
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2011-10-11 17:50:33 +0000

    Noch eine defekte Datei repariert.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1158 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms-publicpersonalprofile/src/com/arsdigita/cms/publicpersonalprofile/PublicPersonalProfilesServlet.java
+++ ccm-cms-publicpersonalprofile/src/com/arsdigita/cms/publicpersonalprofile/PublicPersonalProfilesServlet.java
@@ -22,8 +22,13 @@ import com.arsdigita.cms.contenttypes.GenericPerson;
 import com.arsdigita.cms.contenttypes.GenericPersonContactCollection;
 import com.arsdigita.cms.contenttypes.PublicPersonalProfile;
 import com.arsdigita.cms.contenttypes.PublicPersonalProfileNavItemCollection;
-import com.arsdigita.globalization.GlobalizationHelper;
+import com.arsdigita.cms.contenttypes.PublicPersonalProfileXmlUtil;
+import com.arsdigita.cms.dispatcher.CMSDispatcher;
+import com.arsdigita.cms.dispatcher.ItemResolver;
+import com.arsdigita.cms.publicpersonalprofile.ui.PublicPersonalProfileNavItemsAddForm;
+import com.arsdigita.dispatcher.DispatcherHelper;
 import com.arsdigita.domain.DomainObjectFactory;
+import com.arsdigita.globalization.GlobalizationHelper;
 import com.arsdigita.persistence.DataCollection;
 import com.arsdigita.persistence.DataObject;
 import com.arsdigita.persistence.Session;
@@ -272,7 +277,7 @@ public class PublicPersonalProfilesServlet extends BaseApplicationServlet {
 
                             PublicPersonalProfileNavItemCollection navItems =
                                                                    new PublicPersonalProfileNavItemCollection();
-                            navItems.addLanguageFilter(DispatcherHelper.
+                            navItems.addLanguageFilter(GlobalizationHelper.
                                     getNegotiatedLocale().
                                     getLanguage());
                             navItems.addKeyFilter(navPath);
@@ -290,40 +295,16 @@ public class PublicPersonalProfilesServlet extends BaseApplicationServlet {
                                         final ContentGenerator generator =
                                                                (ContentGenerator) generatorObj;
 
-        Element navHome =
-                navList.newChildElement("nav:category",
-                                        "http://ccm.redhat.com/london/navigation");
-        navHome.addAttribute("AbstractTree", "AbstractTree");
-        navHome.addAttribute("description", "");
-        navHome.addAttribute("id", profile.getID().toString());
-        if (navPath == null) {
-            navHome.addAttribute("isSelected", "true");
-        } else {
-            navHome.addAttribute("isSelected", "false");
-        }
-        navHome.addAttribute("sortKey", "");
-        String homeLabel = homeLabels.get(GlobalizationHelper.getNegotiatedLocale().
-                getLanguage());
-        if (homeLabel == null) {
-            navHome.addAttribute("title", "Home");
-        } else {
-            navHome.addAttribute("title", homeLabel);
-        }
-        navHome.addAttribute("url", String.format("/ccm/%s",
-                                                  profile.getProfileUrl()));
+                                        generator.generateContent(profileElem,
+                                                                  owner,
+                                                                  state);
 
-        //Get the available Navigation items
-        PublicPersonalProfileNavItemCollection navItems =
-                                               new PublicPersonalProfileNavItemCollection();
-        navItems.addLanguageFilter(GlobalizationHelper.getNegotiatedLocale().
-                getLanguage());
-        final Map<String, PublicPersonalProfileNavItem> navItemMap =
-                                                        new HashMap<String, PublicPersonalProfileNavItem>();
-        PublicPersonalProfileNavItem navItem;
-        while (navItems.next()) {
-            navItem = navItems.getNavItem();
-            navItemMap.put(navItem.getKey(), navItem);
-        }
+                                    } else {
+                                        throw new ServletException(String.format(
+                                                "Class '%s' is not a ContentGenerator.",
+                                                navItems.getNavItem().
+                                                getGeneratorClass()));
+                                    }
 
                                 } catch (InstantiationException ex) {
                                     throw new ServletException(
