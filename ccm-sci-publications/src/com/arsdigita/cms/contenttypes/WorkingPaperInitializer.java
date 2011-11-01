abfe02167a70cedd5b0209420bb6e7adae1c4f5c
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2011-11-01 21:25:05 +0000

    Updates an den Traversal-Adaptern für Publikationen.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1218 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-sci-publications/src/com/arsdigita/cms/contenttypes/WorkingPaperInitializer.java
+++ ccm-sci-publications/src/com/arsdigita/cms/contenttypes/WorkingPaperInitializer.java
@@ -42,7 +42,7 @@ public class WorkingPaperInitializer extends ContentTypeInitializer {
 
     @Override
     public String getTraversalXML() {
-        return "/WEB-INF/traversal-adapters/com/arsdigita/cms/contenttypes/Publication.xml";
+        return "/WEB-INF/traversal-adapters/com/arsdigita/cms/contenttypes/WorkingPaper.xml";
     }
 
 }
