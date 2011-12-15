a474d63e3fc5998ce16069b4a2d0a4572b1107bc
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2011-12-15 10:18:03 +0000

    Fehlende Constraint für Primary Key im Update.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1359 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/sql/ccm-cms/default/upgrade/6.6.3-6.6.4/create_publish_lock_table.sql
+++ ccm-cms/sql/ccm-cms/default/upgrade/6.6.3-6.6.4/create_publish_lock_table.sql
@@ -5,5 +5,7 @@ CREATE TABLE cms_publish_lock (
     action character varying(256)
 );
 
+ALTER TABLE ONLY cms_publish_lock
+    ADD CONSTRAINT cms_publis_loc_lock_id_p_8n7d0 PRIMARY KEY (lock_id);
 
 -- ALTER TABLE public.cms_publish_lock OWNER TO ccm;
