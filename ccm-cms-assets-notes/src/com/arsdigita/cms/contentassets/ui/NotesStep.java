21dfb7fec1a9a4ceaca18e17cc66ebd3393747c6
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2012-04-12 10:01:39 +0000

    Notes testet jetzt den Bearbeitungsstatus (Ticket #119)
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1587 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms-assets-notes/src/com/arsdigita/cms/contentassets/ui/NotesStep.java
+++ ccm-cms-assets-notes/src/com/arsdigita/cms/contentassets/ui/NotesStep.java
@@ -55,6 +55,7 @@ public class NotesStep extends SecurityPropertyEditor {
         return m_itemModel.getSelectedItem( ps );
     }
 
+    @Override
     public void register( Page p ) {
         super.register( p );
 
