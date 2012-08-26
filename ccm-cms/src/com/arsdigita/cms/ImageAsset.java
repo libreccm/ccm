dc5e36d368f9e660b90343dd0880d2ce5978af73
quasi <quasi@8810af33-2d31-482b-a856-94f89814c4df>
2012-08-26 04:37:27 +0000

    ImageAsset
    @author eingefügt
    
    git-svn-id: https://svn.libreccm.org/ccm/trunk@1833 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/src/com/arsdigita/cms/ImageAsset.java
+++ ccm-cms/src/com/arsdigita/cms/ImageAsset.java
@@ -52,6 +52,7 @@ import org.apache.log4j.Logger;
  *
  * @author Jack Chung
  * @author Stanislav Freidin
+ * @author Sören Bernstein <sbernstein@quasiweb.de>
  *
  * @version $Id: ImageAsset.java 2225 2011-08-02 18:53:56Z pboy $
  */
