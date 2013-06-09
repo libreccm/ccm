d4f138bc480da468c37967091db244b243fc5318
pb <pb@8810af33-2d31-482b-a856-94f89814c4df>
2013-06-09 17:50:47 +0000

    Anpassung und Vervollständigung ResourceBundles sowie Sprachumschaltung abgeschlossen.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@2196 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms-types-inlinesite/src/com/arsdigita/cms/contenttypes/ui/InlineSitePropertiesStep.java
+++ ccm-cms-types-inlinesite/src/com/arsdigita/cms/contenttypes/ui/InlineSitePropertiesStep.java
@@ -21,12 +21,19 @@ package com.arsdigita.cms.contenttypes.ui;
 import com.arsdigita.cms.contenttypes.InlineSite;
 
 import com.arsdigita.bebop.Component;
+import com.arsdigita.bebop.PageState;
+import com.arsdigita.cms.ContentPage;
+import com.arsdigita.cms.ContentSection;
 import com.arsdigita.cms.ui.authoring.BasicPageForm;
 import com.arsdigita.cms.ItemSelectionModel;
+import com.arsdigita.cms.contenttypes.util.InlineSiteGlobalizationUtil;
 import com.arsdigita.cms.ui.authoring.AuthoringKitWizard;
 import com.arsdigita.cms.ui.authoring.SimpleEditStep;
 import com.arsdigita.toolbox.ui.DomainObjectPropertySheet;
 import com.arsdigita.cms.ui.workflow.WorkflowLockedComponentAccess;
+import com.arsdigita.cms.util.GlobalizationUtil;
+import com.arsdigita.domain.DomainObject;
+import java.text.DateFormat;
 
 
 /**
@@ -65,10 +72,39 @@ public class InlineSitePropertiesStep
                                                           itemModel ) {
         DomainObjectPropertySheet sheet = new DomainObjectPropertySheet( itemModel );
 
-        sheet.add( "Name:", InlineSite.NAME );
-        sheet.add( "Title:", InlineSite.TITLE );
-        sheet.add( "Description:", InlineSite.DESCRIPTION);
-        sheet.add( "URL:", InlineSite.URL );
+        sheet.add(GlobalizationUtil.globalize("cms.contenttypes.ui.title"), 
+                  InlineSite.TITLE);
+        sheet.add(GlobalizationUtil.globalize("cms.contenttypes.ui.name"), 
+                  InlineSite.NAME );
+
+        if (!ContentSection.getConfig().getHideLaunchDate()) {
+            sheet.add(GlobalizationUtil
+                      .globalize("cms.contenttypes.ui.launch_date"),
+                    ContentPage.LAUNCH_DATE,
+                    new DomainObjectPropertySheet.AttributeFormatter() {
+
+                        @Override
+                        public String format(DomainObject item,
+                                String attribute,
+                                PageState state) {
+                            ContentPage page = (ContentPage) item;
+                            if (page.getLaunchDate() != null) {
+                                return DateFormat
+                                       .getDateInstance(DateFormat.LONG)
+                                       .format(page.getLaunchDate());
+                            } else {
+                                return (String) GlobalizationUtil
+                                                .globalize("cms.ui.unknown")
+                                                .localize();
+                            }
+                        }
+                    });
+        }
+        sheet.add( GlobalizationUtil.globalize("cms.contenttypes.ui.summary"), 
+                   InlineSite.DESCRIPTION);
+        sheet.add( InlineSiteGlobalizationUtil
+                       .globalize("cms.contenttypes.ui.inlinesite.url"), 
+                   InlineSite.URL );
 
         return sheet;
     }
