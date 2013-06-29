2d926e350f71c694b8022f0a11f4b077ae0bdd05
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2013-06-29 16:24:35 +0000

    Einbindung FAQ-App in Applications Tab
    git-svn-id: https://svn.libreccm.org/ccm/trunk@2236 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-faq/src/com/arsdigita/faq/FaqAppManager.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2013 Jens Pelzetter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package com.arsdigita.faq;
+
+import com.arsdigita.bebop.BoxPanel;
+import com.arsdigita.bebop.Label;
+import com.arsdigita.globalization.GlobalizedMessage;
+import com.arsdigita.ui.admin.applications.AbstractApplicationManager;
+import com.arsdigita.ui.admin.applications.ApplicationInstanceAwareContainer;
+
+/**
+ *
+ * @author Jens Pelzetter <jens@jp-digital.de>
+ * @version $Id$
+ */
+public class FaqAppManager extends AbstractApplicationManager<Faq> {
+
+    public Class<Faq> getApplication() {
+        return Faq.class;
+    }
+
+    public ApplicationInstanceAwareContainer getApplicationAdminForm() {
+        final ApplicationInstanceAwareContainer container = new ApplicationInstanceAwareContainer();
+
+        final BoxPanel panel = new BoxPanel();
+        panel.add(new Label(new GlobalizedMessage("faq.ui.admin.no_settings", "com.arsdigita.faq.FaqResources")));
+        
+        container.add(panel);
+        
+        return container;
+    }
+    
+    
+    
+}
