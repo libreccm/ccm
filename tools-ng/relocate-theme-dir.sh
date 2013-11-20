790777c5c9d13aae5dd08e9184cbc68ffb2d5ed8
quasi <quasi@8810af33-2d31-482b-a856-94f89814c4df>
2013-11-20 12:57:49 +0000

    Relocate-Theme Script aktualisiert
    git-svn-id: https://svn.libreccm.org/ccm/trunk@2440 8810af33-2d31-482b-a856-94f89814c4df

--- tools-ng/relocate-theme-dir.sh
+++ tools-ng/relocate-theme-dir.sh
@@ -14,6 +14,7 @@ function list_dir() {
 			if [[ -f "${fh}" ]]; then
 				case "${fh}" in
 					*.xsl)	change_xsl_file "${fh}";;
+					*.css)	change_css_file "${fh}";;
 				esac
 			fi
 		fi
@@ -26,7 +27,25 @@ function change_xsl_file() {
 	sed -i "s/__ccm__\///g" "${filename}"
 	sed -i "s/\.\.\/ROOT/themes\/heirloom/g" "${filename}"
 	sed -i "s/http\:\/\/ccm\.redhat\.com\/london\/navigation/http\:\/\/ccm.redhat.com\/navigation/g" "${filename}"
-#	sed -i "s/http\:\/\/ccm\.redhat\.com\/london\/navigation/http\:\/\/ccm.redhat.com\/themedirector/g" "${filename}"
+	sed -i "s/\/ccm-ldn-theme\/themes-dev/\.\.\/\.\.\/devel-themedir/g" "${filename}"
+
+	sed -i "s/ccm-ldn-atoz/heirloom/g" "${filename}"
+	sed -i "s/ccm-forum/heirloom/g" "${filename}"
+	sed -i "s/ccm-ldn-theme\/themes-dev/heirloom/g" "${filename}" #????
+	sed -i "s/ccm-ldn-search/heirloom/g" "${filename}"
+	sed -i "s/ccm-ldn-shortcuts/heirloom/g" "${filename}"
+	sed -i "s/ccm-ldn-subsite/heirloom/g" "${filename}"
+	sed -i "s/ccm-ldn-terms/heirloom/g" "${filename}"
+	sed -i "s/ccm-ldn-theme/heirloom/g" "${filename}"
+#	sed -i "s/ / /g" "${filename}"
+}
+
+function change_css_file() {
+	filename="$1"
+	echo "Change file ${filename}"
+	sed -i "s/__ccm__\/static\/atoz/..\/..\/heirloom\/apps\/atoz\/lib/g" "${filename}"
+	sed -i "s/__ccm__\/static\/navigation/..\/..\/heirloom\/apps\/navigation\/lib/g" "${filename}"
+	sed -i "s/__ccm__\/static\/terms/..\/..\/heirloom\/apps\/terms\/lib/g" "${filename}"
 }
 
 pushd $1
