52d8a5285ec56c02a30c943649f47f466b3e9dfd
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2014-07-10 10:41:32 +0000

    Some JavaDoc for RelationAttributeResourceBundleControl.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@2747 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/src/com/arsdigita/cms/RelationAttributeResourceBundleControl.java
+++ ccm-cms/src/com/arsdigita/cms/RelationAttributeResourceBundleControl.java
@@ -1,17 +1,45 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2014 Jens Pelzetter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
  */
-
 package com.arsdigita.cms;
 
+import com.arsdigita.cms.contenttypes.ui.GenericOrganizationalUnitPersonAddForm;
+import com.arsdigita.globalization.GlobalizedMessage;
 import java.util.ListResourceBundle;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
- *
+ * This implementation of {@link ResourceBundle.Control} is intended to be used for showing 
+ * RelationAttribute values (data base driven enum} values in Bebop Controls using 
+ * {@link GlobalizedMessage}. Used it by creating a {@link GlobalizedMessage} like this:
+ * 
+ * <pre>
+ * new GlobalizedMessage("attribute_key",
+                         "attribute_name",
+                         new RelationAttributeResourceBundleControl());
+ * </pre>
+ * 
+ * Replace <code>attribute_key</code> with the key of the attribute value and 
+ * <code>attribute_name</code> with the name of the relation attribute. 
+ * 
+ * To see this action, please refer for example to {@link GenericOrganizationalUnitPersonAddForm}.
+ * 
  * @author Jens Pelzetter <jens.pelzetter@scientificcms.org>
  */
 public class RelationAttributeResourceBundleControl extends ResourceBundle.Control {
