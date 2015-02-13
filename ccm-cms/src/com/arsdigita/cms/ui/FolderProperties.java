716175b57497aa1eb23707e16909f4a8f034f39a
tosmers <tosmers@8810af33-2d31-482b-a856-94f89814c4df>
2015-02-13 23:48:40 +0000

    [UPDATE] In der Klasse com/arsdigita/cms/ui/FolderProperties.java die FormProcessException in der Zeile 216 umgebaut.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@3166 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/src/com/arsdigita/cms/ui/FolderProperties.java
+++ ccm-cms/src/com/arsdigita/cms/ui/FolderProperties.java
@@ -213,7 +213,8 @@ public class FolderProperties extends CMSContainer {
                         throws FormProcessException {
                         PageState state = event.getPageState();
                         if ( isCancelled(state) ) {
-                            throw new FormProcessException( (String) GlobalizationUtil.globalize("cms.ui.cancel_hit").localize());
+                            throw new FormProcessException(
+                                    GlobalizationUtil.globalize("cms.ui.cancel_hit"));
                         }
                     }
                 });
