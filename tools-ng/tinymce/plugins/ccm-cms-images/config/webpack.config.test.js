a8889c03b0dc6d815efc923c22ef57323791f9c7
baka <baka@8810af33-2d31-482b-a856-94f89814c4df>
2018-12-04 10:04:46 +0000

    Adds a build option to directly compile the plugin into the runtime, so ant deploy is not needed for testing changes.
    git-svn-id: https://svn.libreccm.org/ccm/trunk@5766 8810af33-2d31-482b-a856-94f89814c4df

---
+++ tools-ng/tinymce/plugins/ccm-cms-images/config/webpack.config.test.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const pluginName = "ccm-cms-images";
+
+module.exports = {
+  entry: {
+    plugin: "./src/index.ts",
+    "plugin.min": "./src/index.ts"
+  },
+  output: {
+    path: path.join(__dirname, "../../../../../runtime/apache-tomcat-8.5.15/webapps/ROOT/assets/tinymce/js/tinymce/plugins", pluginName),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: "ts-loader"
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    }),
+    new CopyWebpackPlugin([{
+      from: path.join(__dirname, "../src/LICENSE"),
+      to: path.join(__dirname, "../dist", pluginName)
+    }])
+  ]
+};
